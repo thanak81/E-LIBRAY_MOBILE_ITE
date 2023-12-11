4e9085f7d3301edeea56c266dd3d4e743ee69446
Thanak Mech <thanakmech@gmail.com>
2023-12-11 14:49:38 +0700

    Add view binding

--- app/src/main/java/kh/edu/rupp/ite/e_librar_ite_se1/MainActivity2.kt
+++ app/src/main/java/kh/edu/rupp/ite/e_librar_ite_se1/MainActivity2.kt
@@ -2,10 +2,13 @@ package kh.edu.rupp.ite.e_librar_ite_se1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kh.edu.rupp.ite.e_librar_ite_se1.databinding.ActivityMain2Binding
 
 class MainActivity2 : AppCompatActivity() {
+    private lateinit var binding : ActivityMain2Binding;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        binding = ActivityMain2Binding.inflate(layoutInflater);
+        setContentView(binding.root);
     }
 }
