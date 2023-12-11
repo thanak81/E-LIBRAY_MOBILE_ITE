6418ee452560f18989a77e1e3047e7d096b18add
Thanak Mech <thanakmech@gmail.com>
2023-12-11 14:44:52 +0700

    Prepare stuff

---
+++ app/src/main/java/kh/edu/rupp/ite/e_librar_ite_se1/MainActivity2.kt
@@ -0,0 +1,11 @@
+package kh.edu.rupp.ite.e_librar_ite_se1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
