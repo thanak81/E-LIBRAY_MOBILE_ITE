96aee4c409151b638f1e04842749310453f08522
Thanak Mech <thanakmech@gmail.com>
2023-12-12 17:15:59 +0700

    change fragment from selected to nav_graph

--- app/src/main/java/kh/edu/rupp/ite/e_librar_ite_se1/MainActivity.kt
+++ app/src/main/java/kh/edu/rupp/ite/e_librar_ite_se1/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import kh.edu.rupp.ite.e_librar_ite_se1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,24 +15,9 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater);
         setContentView(binding.root)
 
-        replaceFragment(HomeFragment());
-        binding.bottomNavigationView.setOnItemSelectedListener {
-            when(it.itemId){
-                R.id.home -> replaceFragment(HomeFragment());
-                R.id.favorite-> replaceFragment(FavoriteFragment())
-                else ->{
+        val navController = findNavController(R.id.nav_host_fragment);
+        val bottomNav = binding.bottomNavigationView;
 
-                }
-            }
-
-            true
-        }
-
-    }
-    private fun replaceFragment(fragment: Fragment){
-        val fragmentManager = supportFragmentManager;
-        val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.frameLayout,fragment)
-        fragmentTransaction.commit();
+        bottomNav.setupWithNavController(navController);
     }
 }
